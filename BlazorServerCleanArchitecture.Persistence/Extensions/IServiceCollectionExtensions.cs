13dfca9557388c09691cd3190708755da29d0d31
potlitel <potlitel@gmail.com>
2023-10-02 17:40:38 -0400

    Se crean las clases requeridas en la capa de infraestructura.

---
+++ BlazorServerCleanArchitecture.Persistence/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,41 @@
+﻿using BlazorServerCleanArchitecture.Application.Interfaces.Repositories;
+using BlazorServerCleanArchitecture.Persistence.Contexts;
+using BlazorServerCleanArchitecture.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BlazorServerCleanArchitecture.Persistence.Extensions
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static void AddPersistenceLayer(this IServiceCollection services, IConfiguration configuration)
+        {
+            //services.AddMappings();
+            services.AddDbContext(configuration);
+            services.AddRepositories();
+        }
+
+        //private static void AddMappings(this IServiceCollection services)
+        //{
+        //    services.AddAutoMapper(Assembly.GetExecutingAssembly());
+        //}
+
+        public static void AddDbContext(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+               options.UseSqlServer(connectionString,
+                   builder => builder.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+        }
+
+        private static void AddRepositories(this IServiceCollection services)
+        {
+            services
+                .AddTransient(typeof(IUnitOfWork), typeof(UnitOfWork))
+                .AddTransient(typeof(IGenericRepository<>), typeof(GenericRepository<>))
+                .AddTransient<IStadiumRepository, StadiumRepository>();
+        }
+    }
+}
---
+++ BlazorServerCleanArchitecture.Persistence/Repositories/StadiumRepository.cs
@@ -0,0 +1,21 @@
+﻿using BlazorServerCleanArchitecture.Application.Interfaces.Repositories;
+using BlazorServerCleanArchitecture.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorServerCleanArchitecture.Persistence.Repositories
+{
+    public class StadiumRepository : IStadiumRepository
+    {
+        private readonly IGenericRepository<Stadium> _repository;
+
+        public StadiumRepository(IGenericRepository<Stadium> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<List<Stadium>> GetStadiumByCityAsync(string cityName)
+        {
+            return await _repository.Entities.Where(x => x.City == cityName).ToListAsync();
+        }
+    }
+}
