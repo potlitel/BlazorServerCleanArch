fa7c2e64158ba82ed2c3dda16f78869c6e17976e
potlitel <potlitel@gmail.com>
2023-10-02 17:30:46 -0400

    Se crean las clases requeridas en la capa de aplicación..

---
+++ BlazorServerCleanArchitecture.Application/Features/Stadiums/Queries/GetAllStadiumsDto.cs
@@ -0,0 +1,16 @@
+﻿using BlazorServerCleanArchitecture.Application.Common.Mappings;
+using BlazorServerCleanArchitecture.Domain.Entities;
+
+namespace BlazorServerCleanArchitecture.Application.Features.Stadiums.Queries
+{
+    public class GetAllStadiumsDto : IMapFrom<Stadium>
+    {
+        public int Id { get; init; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public int? Capacity { get; set; }
+        public int? BuiltYear { get; set; }
+        public int? PitchLength { get; set; }
+        public int? PitchWidth { get; set; }
+    }
+}
